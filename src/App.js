b068ee2fd16fe35143dfbacf9adca5f0b23c7e14
Jeffery Foyil <jefferyfoyil@gmail.com>
2022-07-27 13:32:04 -0700

    cors finally working plox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <form>
           <label>
             First Name: {" "}
@@ -39,42 +39,7 @@ function App() {
 }
 
 function sendEmail(firstName, lastName, email){
-  console.log(firstName);
-  console.log(lastName);
-  console.log(email);
-  // npm install createsend-node
-
-  // Authenticate with API Key
-  var createsend = require('createsend-node');
-  var env = require('dotenv');
-  var auth = { apiKey: env.APIKEY };
-  var api = new createsend(auth);
-
-  // Create a details object
-  var details = {};
-
-  // Add the unique identifier for the smart email
-  details.smartEmailID = '8b2c51cc-dbfd-463e-8367-f2322a903eb7';
-
-  // Add the 'To' email address
-  //details.to = "Blake Davis <jeff@longdriveagency.com>";
-  details.to = `${firstName} ${lastName} <${email}>`; 
-
-  // Add mail merge variables
-  details.data = {
-    "variableName": "variableNameTestValue"
-  }
-
-
-  // Send the smart email(and provide a callback function that takes an error and a response parameter)
-  api.transactional.sendSmartEmail(details, function (err, res) {
-      if (err) {
-          /*do something*/
-      } else {
-          /*do something*/
-      }
-  });
+  fetch(`http://localhost:5000/express_backend?firstName=${firstName}&lastName=${lastName}&email=${email}`);
 }
-
 
 export default App;
---
+++ src/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+// This displays message that the server running and listening to specified port
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => { //Line 9
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+
+  // Authenticate with API Key
+  var createsend = require('createsend-node');
+  const dotenv = require("dotenv");
+
+  dotenv.config({ path: ".env" });
+  var auth = { apiKey: process.env.APIKEY };
+  var api = new createsend(auth);
+
+  // Create a details object
+  var details = {};
+
+  // Add the unique identifier for the smart email
+  details.smartEmailID = '8b2c51cc-dbfd-463e-8367-f2322a903eb7';
+
+  // Add the 'To' email address
+  //details.to = "Blake Davis <jeff@longdriveagency.com>";
+  details.to = `${req.query.firstName} ${req.query.lastName} <${req.query.email}>`; 
+
+  // Add mail merge variables
+  details.data = {
+    "variableName": "variableNameTestValue"
+  }
+
+  // Send the smart email(and provide a callback function that takes an error and a response parameter)
+  api.transactional.sendSmartEmail(details, function (err, res) {
+      if (err) {
+          /*do something*/
+          console.log("ERROR")
+      } else {
+          /*do something*/
+          console.log("YOU DID IT")
+      }
+  });
+}); 

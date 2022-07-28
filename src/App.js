7671a8df0fd183dde9be5f87f79b4236ed40791c
Jeffery Foyil <jefferyfoyil@gmail.com>
2022-07-28 15:00:49 -0700

    added navbar for page navigation, will have transactional email and list

--- src/App.js
+++ src/App.js
@@ -2,50 +2,76 @@ import React, {useState} from 'react';
 import './App.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css'; 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import List from './components/Pages/List';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
-  const [startDate, setStartDate] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <form>
-          <label>
-            First Name: {" "}
-            <input type="text" name='firstName' id="firstName" />
-          </label>
-          <br />
-          <label>
-            Last  Name: {" "}
-            <input type="text" name='lastName' id="lastName" />
-          </label>
-          <br />
-          <label>
-            Email: {" "}
-            <input type="text" name='email' id="email" />
-          </label>
-          <br />
-          <label>
-            Last Played:
-          </label>
-          <DatePicker 
-            id='lastPlayed'
-            selected={startDate} 
-            onChange={(date) => setStartDate(date)}
-            maxDate={new Date()} 
-          />
-          <br />
-          <button onClick={() => sendEmail(
-                                            document.getElementById("firstName").value,
-                                            document.getElementById("lastName").value, 
-                                            document.getElementById("email").value,
-                                            document.getElementById("lastPlayed").value)} 
-                  type="button">Submit
-          </button>
-        </form>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/list' element={<List />} />
+      </Routes>
+    </Router>
   );
 }
+
+
+
+const Home = () => {
+
+  const [startDate, setStartDate] = useState(null);
+  return (
+    <div className='App'>
+      <form>
+        <label>
+          First Name: {" "}
+          <input type="text" name='firstName' id="firstName" />
+        </label>
+        <br />
+        <label>
+          Last  Name: {" "}
+          <input type="text" name='lastName' id="lastName" />
+        </label>
+        <br />
+        <label>
+          Email: {" "}
+          <input type="text" name='email' id="email" />
+        </label>
+        <br />
+        <label>
+          Last Played:
+        </label>
+        <DatePicker 
+          id='lastPlayed'
+          selected={startDate} 
+          onChange={(date) => setStartDate(date)}
+          maxDate={new Date()} 
+        />
+        <br />
+        <button onClick={() => sendEmail(
+                                          document.getElementById("firstName").value,
+                                          document.getElementById("lastName").value, 
+                                          document.getElementById("email").value,
+                                          document.getElementById("lastPlayed").value)} 
+                type="button">Submit
+        </button>
+      </form>
+    </div>
+    
+  );
+
+
+
+  
+}
+
 
 function sendEmail(firstName, lastName, email, lastPlayed){
   fetch(`http://localhost:5000/express_backend?firstName=${firstName}&lastName=${lastName}&email=${email}&lastPlayed=${lastPlayed}`)
---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {NavLink, Link} from 'react-router-dom'
+import "../App.css"
+
+
+const Navbar = () => {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+            <div className="container">
+                <img src='https://www.dictionary.com/e/wp-content/uploads/2018/03/Thinking_Face_Emoji-Emoji-Island.png' width='30' height='30' alt='' />
+                <Link to='/' className="fs-3 ubuntu navbar-brand">Testing</Link>
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav" 
+                    aria-controls="navbarNav" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <style jsx="true">
+                        {`
+                        button[aria-expanded="false"] > .close{
+                            display : none;
+                        }
+
+                        button[aria-expanded="true"] > .open{
+                            display : none;
+                        }
+                        `}
+                    </style>
+                    <i className='fas fa-bars open'></i>
+                    <i className='fas fa-times close'></i>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className='navbar-nav fs-5'>
+                        <NavLink activeclassname='active' to='/' className="nav-link">Transactional</NavLink>
+                        <NavLink activeclassname='active' to='/list' className="nav-link">List</NavLink>
+                        
+                    </div> 
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
